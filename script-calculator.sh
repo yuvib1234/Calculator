5b76046c9b3d0f96274c7abe503260d1438de718
yuvib1234 <YBrar@smail.pembinatrails.ca>
2024-10-21 15:01:06 -0500

    Made Calculator Loop
    Made calculator loop after giving you an answer so you dont have to rerun the script every time you want to use the calculator.

--- script-calculator.sh
+++ script-calculator.sh
@@ -1,25 +1,34 @@
-  GNU nano 7.2                                           calculator.sh                                                     
-#Welcome Message
-echo "Welcome to the Calculator!"
-#Reading The Problem
-echo "Enter Two numbers:"
-read a
-read b
-#Method of Math operations
-echo "1. Addition"
-echo "2. Subtraction"
-echo "3. Multiplication"
-echo "4. Divison"
-read ch
+#!/bin/bash
 
-case $ch in
-1)res=`echo $a + $b | bc`
-;;
-2)res=`echo $a - $b | bc`
-;;
-3)res=`echo $a \* $b | bc`
-;;
-4)res=`echo "scale =2; $a / $b" | bc`
-;;
-esac
-echo "Result : $res"
+while true; do
+    echo "Welcome to the Calculator!"
+    
+    # Reading The Problem
+    echo "Enter Two numbers:"
+    read a
+    read b
+    
+    # Method of Math operations
+    echo "Choose an operation:"
+    echo "1. Addition"
+    echo "2. Subtraction"
+    echo "3. Multiplication"
+    echo "4. Division"
+    echo "5. Exit"
+    read ch
+
+    case $ch in
+        1) res=$(echo "$a + $b" | bc);;
+        2) res=$(echo "$a - $b" | bc);;
+        3) res=$(echo "$a * $b" | bc);;
+        4) res=$(echo "scale=2; $a / $b" | bc);;
+        5) echo "Exiting..."; exit 0;;
+        *) echo "Invalid option, please try again.";;
+    esac
+
+    if [[ -n $res ]]; then
+        echo "Result: $res"
+    fi
+
+    echo ""
+done
